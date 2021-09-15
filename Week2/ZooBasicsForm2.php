04433577e9b3fcdb7953c526c0a5d095fddafabe
Emanuel Dobra <ema.dobra@gmail.com>
2021-09-15 18:21:28 -0400

    Updated Week2 notes

--- Week2/ZooBasicsForm2.php
+++ Week2/ZooBasicsForm2.php
@@ -8,14 +8,22 @@ echo "
     <title>Zoo Land</title>
 </head>
 <body>     
-<h1>Heading One</h1>";
+<h1>Heading One</h1>\n";
+// '\n' is used for debugging, as it separates html elements with a new line
 $animalName = $_POST["f_AnimalName"];
 $animalType = $_POST["f_AnimalType"];
 $pounds = $_POST["f_Pounds"];
 $notes = $_POST["f_Notes"];
+// First way to do it
 echo "<p>Animal name: ";
 echo $animalName;
 echo "</p>\n";
+// Concat strings
+echo "<p>Type: " . $animalType . "</p>";
+echo "<p>Food intake: " . $pounds . " pounds per day.</p>";
+// Shortcuts (works for simple taks)
+echo "<p>Details: $notes</p>";
+
 echo "</body>
 </html>
 ";
