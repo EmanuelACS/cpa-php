7f6fa73a1d2ec66f0be1a56020b4d6e4fd5c2772
Emanuel Dobra <ema.dobra@gmail.com>
2021-10-06 17:30:34 -0400

    Added starter week5 notes

---
+++ Week5/include.php
@@ -0,0 +1,21 @@
+function CreateConnectionObject()
+{
+    $fh = fopen('auth.txt','r');
+    $Host =  trim(fgets($fh));
+    $UserName = trim(fgets($fh));
+    $Password = trim(fgets($fh));
+    $Database = trim(fgets($fh));
+    $Port = trim(fgets($fh)); 
+    fclose($fh);
+    $mysqlObj = new mysqli($Host, $UserName, $Password,$Database,$Port);
+    // if the connection and authentication are successful, 
+    // the error number is 0
+    // connect_errno is a public attribute of the mysqli class.
+    if ($mysqlObj->connect_errno != 0) 
+    {
+     echo "<p>Connection failed. Unable to open database $Database. Error: "
+              . $mysqlObj->connect_error . "</p>";
+     // stop executing the php script
+     exit;
+    }
+    return ($mysqlObj);
